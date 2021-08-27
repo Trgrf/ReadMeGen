53a3dce50fa8c420525c879853f710650ad0364c
Theo <trgrf1@gmail.com>
2021-08-27 15:45:54 -0400

    saving changes to my index.js and gen.js

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What type of license are you using?',
+        choices: ['MIT', '']
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is description of your project?'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Install Instructions?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage of your application?'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Did you collaborate on this project or use other resources list the links here.'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Have you run tests, if so provide how to run them.'
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+   fs.writeFile('README.md', generateMarkdown(data), (err)) 
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions).then(generateMarkdown(data))
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `www.github.com/license/${license}/`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,7 +17,27 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description
+${data.description}.
 
+## Table of Contents (Optional)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+## Installation
+${data.install}.
+## Usage
+${data.usage}.
+## License
+${data.license}.
+## Credits
+${data.credits}.
+## Tests
+${data.test}.
+## Questions
+If you are interested in contacting me my GitHub profile is ${data.github}. <br />
+My email is ${data.email}.
 `;
 }
 
